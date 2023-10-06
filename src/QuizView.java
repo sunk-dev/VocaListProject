2e85b47d43181e768ddd008fce2450bb558fe4d5
seon <sunkyoung.dev@gmail.com>
2023-10-06 14:49:54 +0900

    젠체 QuizView(주관식 객관식 만드는) 생성

---
+++ src/QuizView.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class QuizView {
+    private final WordRepository wr;
+
+    public QuizView() throws IOException {
+        this.wr=new WordRepository();
+
+    }
+}
