1a10fad8799e1adab7f164d0169b98a249ab8803
seon <sunkyoung.dev@gmail.com>
2023-10-06 14:17:13 +0900

    단어불러오기 완료

---
+++ src/WordRepository.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class WordRepository {
+    public static void main(String[] args) throws IOException {
+        Map<String, String> wordList = new HashMap<String, String>();
+        File note = new File("D:\\VocaList\\testword.txt");
+        BufferedReader br = new BufferedReader(new FileReader(note));
+        String str = br.readLine();
+        while(str != null){ // 읽어오는 문자열이 null이 나올때까지 루프
+            String[]word=str.split("/");
+            wordList.put(word[0],word[1]);
+            str = br.readLine(); // 메모장 다음 문자열 읽기
+        }
+    }
+
+
+}
