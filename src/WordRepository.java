9caf7afd1a37bc2c69e16ee113f6fc14cae1a7af
seon <sunkyoung.dev@gmail.com>
2023-10-06 14:45:46 +0900

    단어불러오기 클래스

--- src/WordRepository.java
+++ src/WordRepository.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 
 public class WordRepository {
-    public static void main(String[] args) throws IOException {
+    public WordRepository() throws IOException {
         Map<String, String> wordList = new HashMap<String, String>();
         File note = new File("D:\\VocaList\\testword.txt");
         BufferedReader br = new BufferedReader(new FileReader(note));
@@ -15,6 +15,4 @@ public class WordRepository {
             str = br.readLine(); // 메모장 다음 문자열 읽기
         }
     }
-
-
 }
