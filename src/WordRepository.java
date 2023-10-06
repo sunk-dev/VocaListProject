d69653c0f0cebc033c009347b9bcca656a419db5
seon <sunkyoung.dev@gmail.com>
2023-10-06 14:49:24 +0900

    WordRepository 변수이름 수정

--- src/WordRepository.java
+++ src/WordRepository.java
@@ -6,8 +6,8 @@ import java.util.Map;
 public class WordRepository {
     public WordRepository() throws IOException {
         Map<String, String> wordList = new HashMap<String, String>();
-        File note = new File("D:\\VocaList\\testword.txt");
-        BufferedReader br = new BufferedReader(new FileReader(note));
+        File wordFile = new File("D:\\VocaList\\testword.txt");
+        BufferedReader br = new BufferedReader(new FileReader(wordFile));
         String str = br.readLine();
         while(str != null){ // 읽어오는 문자열이 null이 나올때까지 루프
             String[]word=str.split("/");
